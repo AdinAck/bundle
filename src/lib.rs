0dc30b47ccaf8837dece7dc5d663592df6016c23
Adin Ackerman <adinackerman@gmail.com>
2023-11-01 14:45:18 -0700

    fixed typo

--- src/lib.rs
+++ src/lib.rs
@@ -134,7 +134,7 @@ pub fn bundle(input: TokenStream) -> TokenStream {
         #[allow(unused)]
         macro_rules! #use_macro_name {
             ( $BUNDLE:ident, $IDENT:ident, $CODE:block ) => {
-                match self {
+                match $BUNDLE {
                     #(
                         #name::#types($IDENT) => $CODE
                     ),*
